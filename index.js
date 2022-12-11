778b25cc24131659b28b08b80b911802333c4d01
hassanabdinasir4 <hassan.hassan@student.moringaschool.com>
2022-12-11 14:06:47 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+var dodger = document.getElementById('dodger');
+
+function moveDodgerLeft(){
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers,10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight(){
+    var leftNumbers = dodger.style.left.replace(`px` , ``);
+    var left = parseInt(leftNumbers, 10);
+
+    if(left < 360){
+        dodger.style.left = `${left + 1}px`
+    }
+}
+
+document.addEventListener(`keydown`,function(e){
+    if (e.key === 37) {
+        moveDodgerLeft();
+    }
+    if ( e.key === 39) {
+        moveDodgerRight();
+    }
+});
+
+
+
